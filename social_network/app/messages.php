29d8aa21a06aeecb0189b8aad0c07f77958801ac
ngavuong <vuongnga159@gmail.com>
2019-03-30 21:07:26 +0700

    create migration and model users, friends, messages

---
+++ social_network/app/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class messages extends Model
+{
+    //
+     protected $table = 'messages';
+    public $timestamps = false;
+
+    public function toUsers{
+    	return $this->belongstoMany('App\users');
+    }
+
+    public function fromUser{
+    	return $this->belongsto('App\users');
+    }
+}
