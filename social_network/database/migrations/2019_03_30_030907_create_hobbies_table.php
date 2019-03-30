f59a3321d03d44c3c9f5a2e46584058a959372df
bagiadaukho <hoangchungbung@gmail.com>
2019-03-30 10:13:09 +0700

    Create Migration Hobbies

---
+++ social_network/database/migrations/2019_03_30_030907_create_hobbies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHobbiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hobbies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $tabe->text('hobbie')->nullable();
+            $table->text('movies')->nullable();
+            $table->text('books')->nullable();
+            $table->text('other')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hobbies');
+    }
+}
