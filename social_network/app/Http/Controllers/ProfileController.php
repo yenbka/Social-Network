5030a391574c1603ace9c6e7f812d2094b304426
Son-Dang-Dinh <sondangdinh1998@gmail.com>
2019-04-28 22:14:28 +0700

    thêm method index trong profile controller để trả về dữ liệu

--- social_network/app/Http/Controllers/ProfileController.php
+++ social_network/app/Http/Controllers/ProfileController.php
@@ -7,4 +7,26 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     //
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function secure($id){
+        $user = User::where('id', $id)->first();
+
+        if ($user){
+            $is_my_profile = (Auth::id() == $id)?true:false;
+            if (!$is_my_profile){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function index($id) {
+        if (!$this->secure($id)) return redirect('/404');
+        $profile_info = Profile::where('id', $id)->first();
+        return view('profile', ['profile_info'=>$profile_info]);
+    }
 }
