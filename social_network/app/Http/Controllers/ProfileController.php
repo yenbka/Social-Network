ac01bd45d51527097fad58ef565877df8f957a4f
Son-Dang-Dinh <sondangdinh1998@gmail.com>
2019-04-28 23:48:33 +0700

    thêm method profile controller cập nhật personal information

--- social_network/app/Http/Controllers/ProfileController.php
+++ social_network/app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use Auth;
 use App\Profile;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
@@ -37,5 +38,45 @@ class ProfileController extends Controller
         $user = Auth::user();
         $profile = Profile::where('id', $user->id)->first();
         return view('profile_save_info', ['user' => $user, 'profile' => $profile]);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+        ]);
+    }
+
+    protected function update(array $data, $id) {
+        User::where('id', $id)->update([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'email' => $data['email'],
+        ]);
+
+        Profile::where('id', $id)->update([
+            'about_me' => $data['about_me'],
+            'birth_date' => $data['birth_date'],
+            'address' => $data['address'],
+            'gender' => $data['gender'],
+            'phone' => $data['phone'],
+            // 'status' => $data['status'],
+        ]);
+    }
+
+    public function profile_save_info(Request $request, $id){
+        $allRequest  = $request->all();	
+        $validator = $this->validator($allRequest);
+        if ($validator->fails()) {
+            // Dữ liệu vào không thỏa điều kiện sẽ thông báo lỗi
+            return view('profile_save_info', ['user' => Auth::user(), 'profile' => Profile::find($id)])->withErrors($validator)->withInput();
+        } else {   
+            // Dữ liệu vào hợp lệ sẽ thực hiện tạo người dùng dưới csdl
+            $this->update($allRequest, $id);
+            // Insert thành công sẽ hiển thị thông báo
+            $status = "Cập nhật thông tin cá nhân thành công!";
+            return view('profile_save_info', ['user' => Auth::user(), 'profile' => Profile::find($id), 'status' => $status]);           
+        }
     }
 }
