2c1e7373cd4765c3d787f12fa7c96ea4fc51bba1
Son-Dang-Dinh <sondangdinh1998@gmail.com>
2019-04-28 22:53:01 +0700

    thêm file widget, chỉnh sửa giao diện trang about

--- social_network/resources/views/about.blade.php
+++ social_network/resources/views/about.blade.php
@@ -26,18 +26,18 @@
 						<div class="row">
 							<div class="col col-lg-6 col-md-6 col-sm-12 col-12">
 
-{{--								@include('partials.widgets.w-personal-info1')--}}
+								@include('partials.widgets.w-personal-info1')
 
 							</div>
 							<div class="col col-lg-6 col-md-6 col-sm-12 col-12">
 
-{{--								@include('partials.widgets.w-personal-info2')--}}
+								@include('partials.widgets.w-personal-info2')
 
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="ui-block">
+				<!-- <div class="ui-block">
 					<div class="ui-block-title">
 						<h6 class="title">Education and Employement</h6>
 						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg></a>
@@ -56,7 +56,7 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</div>
 
 			<div class="col col-xl-4 order-xl-1 col-lg-4 order-lg-1 col-md-12 order-md-2 col-sm-12 col-12">
@@ -67,7 +67,7 @@
 					</div>
 					<div class="ui-block-content">
 
-{{--						@include('partials.widgets.w-personal-info5')--}}
+						@include('partials.widgets.w-personal-info5')
 
 						@include('partials.widgets.w-socials')
 
