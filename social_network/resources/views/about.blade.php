cd6daef4ec68bc3e1ffde6957d9a4f38803eacda
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-04-29 20:59:34 +0700

    xoá ui không cần thiết

--- social_network/resources/views/about.blade.php
+++ social_network/resources/views/about.blade.php
@@ -37,26 +37,6 @@
 						</div>
 					</div>
 				</div>
-				<!-- <div class="ui-block">
-					<div class="ui-block-title">
-						<h6 class="title">Education and Employement</h6>
-						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg></a>
-					</div>
-					<div class="ui-block-content">
-						<div class="row">
-							<div class="col col-lg-6 col-md-6 col-sm-12 col-12">
-
-{{--								@include('partials.widgets.w-personal-info3')--}}
-
-							</div>
-							<div class="col col-lg-6 col-md-6 col-sm-12 col-12">
-
-{{--								@include('partials.widgets.w-personal-info4')--}}
-
-							</div>
-						</div>
-					</div>
-				</div> -->
 			</div>
 
 			<div class="col col-xl-4 order-xl-1 col-lg-4 order-lg-1 col-md-12 order-md-2 col-sm-12 col-12">
@@ -68,8 +48,6 @@
 					<div class="ui-block-content">
 
 						@include('partials.widgets.w-personal-info5')
-
-						@include('partials.widgets.w-socials')
 
 					</div>
 				</div>
