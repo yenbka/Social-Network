35767b0878a8ca2d2808fed7418844dbc604b809
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-04-29 20:59:10 +0700

    sửa about controller

--- social_network/app/Http/Controllers/AboutController.php
+++ social_network/app/Http/Controllers/AboutController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Profile;
 use App\User;
-// use App\Hobbie;
+use App\Hobbie;
 
 class AboutController extends Controller
 {
@@ -28,9 +28,9 @@ class AboutController extends Controller
     }
 
     public function index($id) {
-        $profile = Profile::where('id', $id)->first();
         $user = User::where('id', $id)->first();
-        // $hobby = Hobbie::where('id', $id)->first();
-        return view('about', ['profile'=>$profile, 'user' => $user]);
+        $profile = Profile::where('id', $user->profile_id)->first();
+        $hobbies = Hobbie::where('id', $user->hobbies_id)->first();
+        return view('about', ['profile'=>$profile, 'user' => $user, 'hobbies'=>$hobbies]);
     }
 }
