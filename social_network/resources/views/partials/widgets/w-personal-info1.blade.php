701a06fece9cbda5171d0e38bc2fd7375cfdc116
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-04-29 21:00:41 +0700

    hiện thoogn tin hobbies

--- social_network/resources/views/partials/widgets/w-personal-info1.blade.php
+++ social_network/resources/views/partials/widgets/w-personal-info1.blade.php
@@ -4,21 +4,11 @@
 <ul class="widget w-personal-info item-block">
 	<li>
 		<span class="title">Hobbies:</span>
-		<span class="text">I like to ride the bike to work, swimming, and working out. I also like
-								reading design magazines, go to museums, and binge watching a good tv show while it’s raining outside.
-							</span>
-	</li>
-	<li>
-		<span class="title">Favourite TV Shows:</span>
-		<span class="text">Breaking Good, RedDevil, People of Interest, The Running Dead, Found,  American Guy.</span>
+		<span class="text">{{$hobbies->hobbie}}</span>
 	</li>
 	<li>
 		<span class="title">Favourite Movies:</span>
-		<span class="text">Idiocratic, The Scarred Wizard and the Fire Crown,  Crime Squad, Ferrum Man. </span>
-	</li>
-	<li>
-		<span class="title">Favourite Games:</span>
-		<span class="text">The First of Us, Assassin’s Squad, Dark Assylum, NMAK16, Last Cause 4, Grand Snatch Auto. </span>
+		<span class="text">{{$hobbies->movies}}</span>
 	</li>
 </ul>
 
