d395013dc79cacab1ab38c7546b0351f99480739
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-04-30 18:46:33 +0700

    xoá thành phần không cần thiết

--- social_network/resources/views/partials/headers-navigations/your-profile.blade.php
+++ social_network/resources/views/partials/headers-navigations/your-profile.blade.php
@@ -33,27 +33,6 @@
 			</div>
 		</div>
 	</div>
-
-	<div class="ui-block-title">
-		<a href="33-YourAccount-Notifications.html" class="h6 title">Notifications</a>
-		<a href="#" class="items-round-little bg-primary">8</a>
-	</div>
-	<div class="ui-block-title">
-		<a href="34-YourAccount-ChatMessages.html" class="h6 title">Chat / Messages</a>
-	</div>
-	<div class="ui-block-title">
-		<a href="35-YourAccount-FriendsRequests.html" class="h6 title">Friend Requests</a>
-		<a href="#" class="items-round-little bg-blue">4</a>
-	</div>
-	<div class="ui-block-title ui-block-title-small">
-		<h6 class="title">FAVOURITE PAGE</h6>
-	</div>
-	<div class="ui-block-title">
-		<a href="36-FavPage-SettingsAndCreatePopup.html" class="h6 title">Create Fav Page</a>
-	</div>
-	<div class="ui-block-title">
-		<a href="36-FavPage-SettingsAndCreatePopup.html" class="h6 title">Fav Page Settings</a>
-	</div>
 </div>
 
 <!-- ... end Your Profile  -->
