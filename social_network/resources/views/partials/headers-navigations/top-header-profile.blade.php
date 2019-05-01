ad8687eca5ddc2899d4018972ad4865ce76901ae
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-05-01 23:25:37 +0700

    thêm dường dẫn tới photo

--- social_network/resources/views/partials/headers-navigations/top-header-profile.blade.php
+++ social_network/resources/views/partials/headers-navigations/top-header-profile.blade.php
@@ -40,7 +40,11 @@
 							<div class="col col-lg-5 ml-auto col-md-5 col-sm-12 col-12">
 								<ul class="profile-menu">
 									<li>
-										<a href="07-ProfilePage-Photos.html">Photos</a>
+										@if(\Request::route()->getName()=='photo')
+											<a href="{{route('photo', ['id' => Auth::id()])}}" class="active">Photos</a>
+										@else
+											<a href="{{route('photo', ['id' => Auth::id()])}}">Photos</a>
+										@endif
 									</li>
 									<li>
 										<a href="09-ProfilePage-Videos.html">Videos</a>
