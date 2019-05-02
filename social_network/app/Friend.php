407ffb2145146549df3f958854f0a045fccf14bc
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-05-02 10:18:19 +0700

    thêm fillable

--- social_network/app/Friend.php
+++ social_network/app/Friend.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Friend extends Model
 {
-    //
     protected $table = 'friends';
+    protected $primaryKey = 'id';
     public $timestamps = false;
-
+    protected $fillable = [
+        'user_id_1',
+        'user_id_2',
+    ];
     public function users(){
     	return $this->belongstoMany('App\users');
     }
