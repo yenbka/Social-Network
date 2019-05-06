0fa5d11bc8dc155b727ffcd374c0203adc53ad80
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-05-06 19:28:08 +0700

    thay các thành phần phù hợp

--- social_network/resources/views/newsfeed.blade.php
+++ social_network/resources/views/newsfeed.blade.php
@@ -42,18 +42,9 @@
 					</div>
 					@endif
 				@endforeach
-					
-					<!-- <div class="ui-block">
-						@include('partials.posts.posts8-BP')
-					</div>
-
-					<div class="ui-block">
-						@include('partials.posts.posts9-BP')
-					</div> -->
-
 				</div>
 
-				<a id="load-more-button" href="#" class="btn btn-control btn-more" data-load-link="items-to-load.html" data-container="newsfeed-items-grid"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg></a>
+				<a id="load-more-button" href="#" class="btn btn-control btn-more" data-load-link="items-to-load.html" data-container="newsfeed-items-grid"><svg class="olymp-three-dots-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-three-dots-icon')}}"></use></svg></a>
 
 			</main>
 
@@ -62,25 +53,31 @@
 
 			<!-- Left Sidebar -->
 
-			<aside class="col col-xl-3 order-xl-1 col-lg-6 order-lg-2 col-md-6 col-sm-12 col-12">
-				<div class="ui-block">
-					{{--@include('partials.widgets.w-weather')--}}
-				</div>
+			<div class="col col-xl-3 order-xl-1 col-lg-6 order-lg-2 col-md-6 col-sm-12 col-12">
 
 				<div class="ui-block">
-{{--					@include('partials.widgets.w-calendar')--}}
+					<div class="ui-block-title">
+						<h6 class="title">Profile Intro</h6>
+					</div>
+					<div class="ui-block-content">
+
+						@include('partials.widgets.w-personal-info')
+
+					</div>
 				</div>
 
 				<div class="ui-block">
 					<div class="ui-block-title">
-						<h6 class="title">Pages You May Like</h6>
-						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg></a>
+						<h6 class="title">Last Videos</h6>
 					</div>
+					<div class="ui-block-content">
 
-					@include('partials.widgets.w-friend-pages-added')
+						@include('partials.widgets.w-last-video')
 
+					</div>
 				</div>
-			</aside>
+
+			</div>
 
 			<!-- ... end Left Sidebar -->
 
@@ -88,37 +85,24 @@
 			<!-- Right Sidebar -->
 
 			<aside class="col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-12 col-12">
-
-				<div class="ui-block">
-{{--					@include('partials.widgets.w-birthsday-alert')--}}
-				</div>
-
 				<div class="ui-block">
 					<div class="ui-block-title">
 						<h6 class="title">Friend Suggestions</h6>
-						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg></a>
+						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-three-dots-icon')}}"></use></svg></a>
 					</div>
-
-{{--					@include('partials.widgets.w-friend-pages-added-suggestions')--}}
-
 				</div>
 
 				<div class="ui-block">
 
 					<div class="ui-block-title">
 						<h6 class="title">Activity Feed</h6>
-						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg></a>
+						<a href="#" class="more"><svg class="olymp-three-dots-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-three-dots-icon')}}"></use></svg></a>
 					</div>
-
-{{--					@include('partials.widgets.w-activity-feed')--}}
 
 				</div>
 
 
 				<div class="ui-block">
-
-{{--					@include('partials.widgets.w-action')--}}
-
 				</div>
 
 			</aside>
