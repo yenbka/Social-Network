913129f3c503e255ca6660c092c3a34d19397f83
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-05-06 19:15:14 +0700

    sửa các đường dẫn

--- social_network/resources/views/partials/forms/news-feed-form.blade.php
+++ social_network/resources/views/partials/forms/news-feed-form.blade.php
@@ -7,7 +7,7 @@
         <li class="nav-item">
             <a class="nav-link active inline-items" data-toggle="tab" href="#home-1" role="tab" aria-expanded="true">
 
-                <svg class="olymp-status-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-status-icon"></use></svg>
+                <svg class="olymp-status-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-status-icon')}}"></use></svg>
 
                 <span>Status</span>
             </a>
@@ -15,17 +15,9 @@
         <li class="nav-item">
             <a class="nav-link inline-items" data-toggle="tab" href="#profile-1" role="tab" aria-expanded="false">
 
-                <svg class="olymp-multimedia-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-multimedia-icon"></use></svg>
+                <svg class="olymp-multimedia-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-multimedia-icon')}}"></use></svg>
 
                 <span>Multimedia</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link inline-items" data-toggle="tab" href="#blog" role="tab" aria-expanded="false">
-                <svg class="olymp-blog-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-blog-icon"></use></svg>
-
-                <span>Blog Post</span>
             </a>
         </li>
     </ul>
@@ -35,8 +27,8 @@
         <div class="tab-pane active" id="home-1" role="tabpanel" aria-expanded="true">
             <form action="{{route('add-post')}}" method="POST" enctype="multipart/form-data" id="post-form">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <div class="post__author author">
-                    <img src="/{{$profile->avatar_path}}" alt="author">
+                <div class="author-thumb">
+                    <img src="{{asset($profile->avatar_path)}}" alt="author" height="36px" width="36px">
                 </div>
                 <div class="form-group with-icon label-floating is-empty">
                     <label class="control-label">Share what you are thinking here...</label>
@@ -48,14 +40,14 @@
                 </div>
                 <div class="add-options-message">
                     <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="ADD PHOTOS">
-                        <svg class="olymp-camera-icon" data-toggle="modal" data-target="#post-photo"><use xlink:href="svg-icons/sprites/icons.svg#olymp-camera-icon"></use></svg>
+                        <svg class="olymp-camera-icon" data-toggle="modal" data-target="#post-photo"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-camera-icon')}}"></use></svg>
                     </a>
                     <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="TAG YOUR FRIENDS">
-                        <svg class="olymp-computer-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-computer-icon"></use></svg>
+                        <svg class="olymp-computer-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-computer-icon')}}"></use></svg>
                     </a>
 
                     <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="ADD LOCATION">
-                        <svg class="olymp-small-pin-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-small-pin-icon"></use></svg>
+                        <svg class="olymp-small-pin-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-small-pin-icon')}}"></use></svg>
                     </a>
 
                     <button type="submit" class="btn btn-primary btn-md-2">Post Status</button>
@@ -69,7 +61,7 @@
         <div class="tab-pane" id="profile-1" role="tabpanel" aria-expanded="true">
             <form>
                 <div class="author-thumb">
-                    <img src="{{asset('images/author-page.jpg')}}" alt="author">
+                    <img src="{{asset($profile->avatar_path)}}" alt="author" height="36px" width="36px">
                 </div>
                 <div class="form-group with-icon label-floating is-empty">
                     <label class="control-label">Share what you are thinking here...</label>
@@ -77,43 +69,14 @@
                 </div>
                 <div class="add-options-message">
                     <a href="#post-photo" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="ADD PHOTOS" data-target="#post-photo">
-                        <svg class="olymp-camera-icon" data-toggle="modal" ><use xlink:href="svg-icons/sprites/icons.svg#olymp-camera-icon"></use></svg>
+                        <svg class="olymp-camera-icon" data-toggle="modal" ><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-camera-icon')}}"></use></svg>
                     </a>
                     <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="TAG YOUR FRIENDS">
-                        <svg class="olymp-computer-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-computer-icon"></use></svg>
+                        <svg class="olymp-computer-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-computer-icon')}}"></use></svg>
                     </a>
 
                     <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="ADD LOCATION">
-                        <svg class="olymp-small-pin-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-small-pin-icon"></use></svg>
-                    </a>
-
-                    <button class="btn btn-primary btn-md-2">Post Status</button>
-                    <button   class="btn btn-md-2 btn-border-think btn-transparent c-grey">Preview</button>
-
-                </div>
-
-            </form>
-        </div>
-
-        <div class="tab-pane" id="blog" role="tabpanel" aria-expanded="true">
-            <form>
-                <div class="author-thumb">
-                    <img src="{{asset('images/author-page.jpg')}}" alt="author">
-                </div>
-                <div class="form-group with-icon label-floating is-empty">
-                    <label class="control-label">Share what you are thinking here...</label>
-                    <textarea class="form-control" name="text" placeholder=""  ></textarea>
-                </div>
-                <div class="add-options-message">
-                    <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="ADD PHOTOS">
-                        <svg class="olymp-camera-icon" data-toggle="modal" data-target="#post-photo"><use xlink:href="svg-icons/sprites/icons.svg#olymp-camera-icon"></use></svg>
-                    </a>
-                    <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="TAG YOUR FRIENDS">
-                        <svg class="olymp-computer-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-computer-icon"></use></svg>
-                    </a>
-
-                    <a href="#" class="options-message" data-toggle="tooltip" data-placement="top"   data-original-title="ADD LOCATION">
-                        <svg class="olymp-small-pin-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-small-pin-icon"></use></svg>
+                        <svg class="olymp-small-pin-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-small-pin-icon')}}"></use></svg>
                     </a>
 
                     <button class="btn btn-primary btn-md-2">Post Status</button>
