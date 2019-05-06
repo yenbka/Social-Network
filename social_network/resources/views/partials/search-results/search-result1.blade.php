703bf855097a181fe83abc1ebecbd0f27494ec45
Son-Dang-Dinh <sondangdinh1998@gmail.com>
2019-05-06 22:39:17 +0700

    bổ sung hàm ajax xử lý sự kiện bấm nút add friend

--- social_network/resources/views/partials/search-results/search-result1.blade.php
+++ social_network/resources/views/partials/search-results/search-result1.blade.php
@@ -11,7 +11,7 @@
 		</div>
 
 		<span class="notification-icon">
-			<a href="#" class="accept-request" onclick="send_request({{$user_result[$i]->id}})">
+			<a id="add" href="#" class="accept-request" onclick="send_request({{$user_result[$i]->id}})">
 				<span class="icon-add without-text">
 					<svg class="olymp-happy-face-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-happy-face-icon"></use></svg>
 				</span>
@@ -21,6 +21,8 @@
 			<!-- <a href="#" class="accept-request chat-message">
 				<svg class="olymp-chat---messages-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-chat---messages-icon"></use></svg>
 			</a> -->
+			<button id="sent" type="button" class="btn btn-success d-none">Friend Request Sent</button>
+			<button id="error" type="button" class="btn btn-warning d-none">Something went wrong!</button>
 		</span>
 
 		<!-- <div class="more">
@@ -109,3 +111,34 @@
 
 </article>
 <!-- ... end Search Result -->
+<script>
+function send_request(id){
+
+	var data = new FormData();
+	data.append('request_id', id);
+
+	$.ajax({
+		url: BASE_URL + '/friend/send_request',
+		type: "POST",
+		timeout: 5000,
+		data: data,
+		contentType: false,
+		cache: false,
+		processData: false,
+		headers: {'X-CSRF-TOKEN': CSRF},
+		success: function (response) {
+			if (response.code == 200) {
+				$("#add").addClass("d-none");
+				$("#sent").removeClass("d-none");
+			} else {
+				$("#add").addClass("d-none");
+				$("#error").removeClass("d-none");
+			}
+		},
+		error: function () {
+			$("#add").addClass("d-none");
+			$("#error").removeClass("d-none");
+		}
+	});
+}
+</script>
