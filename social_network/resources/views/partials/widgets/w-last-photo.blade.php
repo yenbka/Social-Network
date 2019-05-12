9a3de3bdeb499142f4bba4858a81f5b11d36646c
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-05-12 14:46:14 +0700

    xoá thành phần không cần thiết của last photo

--- social_network/resources/views/partials/widgets/w-last-photo.blade.php
+++ social_network/resources/views/partials/widgets/w-last-photo.blade.php
@@ -6,47 +6,6 @@
 			<img src="{{asset('images/last-photo10-large.jpg')}}" alt="photo">
 		</a>
 	</li>
-	<li>
-		<a href="{{asset('images/last-phot11-large.jpg')}}">
-			<img src="{{asset('images/last-phot11-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot12-large.jpg')}}">
-			<img src="{{asset('images/last-phot12-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot13-large.jpg')}}">
-			<img src="{{asset('images/last-phot13-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot14-large.jpg')}}">
-			<img src="{{asset('images/last-phot14-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot15-large.jpg')}}">
-			<img src="{{asset('images/last-phot15-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot16-large.jpg')}}">
-			<img src="{{asset('images/last-phot16-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot17-large.jpg')}}">
-			<img src="{{asset('images/last-phot17-large.jpg')}}" alt="photo">
-		</a>
-	</li>
-	<li>
-		<a href="{{asset('images/last-phot18-large.jpg')}}">
-			<img src="{{asset('images/last-phot18-large.jpg')}}" alt="photo">
-		</a>
-	</li>
 </ul>
-
 
 <!-- .. end W-Latest-Photo -->
