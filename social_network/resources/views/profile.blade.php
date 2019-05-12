92b33208121e17a42b6f7d0ec14774b6ae9c8014
Hoàng Thế Sơn <son.hust20166668@gmail.com>
2019-05-12 21:20:42 +0700

    fix bug undefine type

--- social_network/resources/views/profile.blade.php
+++ social_network/resources/views/profile.blade.php
@@ -19,40 +19,31 @@
 		<div class="row">
 
 			<!-- Main Content -->
-
 			<main class="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
 				<div class="ui-block">
 					@include('partials.forms.news-feed-form')
 				</div>
-
 				<div id="newsfeed-items-grid">
 					@foreach($posts as $post)
-
 						@if($post->has_medias===0)
 							<div class="ui-block">
 							@include('partials.posts.posts6-BP')
 							@include('partials.comments.comment-list2',['comments' => $post->comment, 'post_id' => $post->id])
 							<!-- <a href="#" class="more-comments">View more comments <span>+</span></a> -->
 								@include('partials.forms.comment-form')
-
 							</div>
-						@elseif($post->media->type===1)
+						@else
 							<div class="ui-block">
 							@include('partials.posts.posts7-BP')
 							@include('partials.comments.comment-list2',['comments' => $post->comment, 'post_id' => $post->id])
 							<!-- <a href="#" class="more-comments">View more comments <span>+</span></a> -->
 								@include('partials.forms.comment-form')
 							</div>
-						@else
-							<div class="ui-block">
-								@include('partials.posts.posts5-BP')
-							</div>
 						@endif
+
 					@endforeach
 				</div>
-
 				<a id="load-more-button" href="#" class="btn btn-control btn-more" data-load-link="items-to-load.html" data-container="newsfeed-items-grid"><svg class="olymp-three-dots-icon"><use xlink:href="{{asset('svg-icons/sprites/icons.svg#olymp-three-dots-icon')}}"></use></svg></a>
-
 			</main>
 
 			<!-- ... end Main Content -->
